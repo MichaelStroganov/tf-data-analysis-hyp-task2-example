97e8de10b46d1295c6a1ace6e7b5447a7694c731
MichaelStroganov <90139406+MichaelStroganov@users.noreply.github.com>
2023-04-11 22:43:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy.stats import ks_2samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1004085803 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    statistic, pvalue = ks_2samp(x, y)
+    alpha = 0.02
+    if pvalue > alpha:
+        return False
+    else:
+        True
+
+    
